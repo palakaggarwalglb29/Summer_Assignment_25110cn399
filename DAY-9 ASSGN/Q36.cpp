6ada87188ceb8361280ece96a8f6ec2e2cbd48f4
Palak Aggarwal <palakagg2007@gmail.com>
2026-06-10 16:12:46 +0530

    Add files via upload

---
+++ DAY-9 ASSGN/Q36.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size;
+
+    
+    cout << "Enter the side length of the square: ";
+    cin >> size;
+
+    
+    for (int i = 1; i <= size; i++) {
+        
+        for (int j = 1; j <= size; j++) {
+            // Print asterisk if it is part of the border
+            if (i == 1 || i == size || j == 1 || j == size) {
+                cout << "* ";
+            } else {
+                // Print spaces for the hollow interior
+                cout << "  "; 
+            }
+        }
+        
+        cout << endl;
+    }
+
+
+}
